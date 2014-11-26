4575edd6e3ecf1113d9c097288c8f098a952e5eb
XDdrummer <cathamster7@gmail.com>
2014-11-26 02:04:59 -0500

    Update and rename basicBot7.js to basicBot8.js

--- basicBot8.js
+++ basicBot8.js
@@ -1247,17 +1247,8 @@
                     if( !basicBot.commands.executable(this.rank, chat) ) return void (0);
                     else{
                         
-                            var name = msg.substring(space + 2);
-                            var user = basicBot.userUtilities.lookupUserName(name);
-                            if (user === false || !user.inRoom) {
-                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
-                            }
-                            else if (user.username === chat.un) {
-                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
-                            }
-                            else {
-                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
-                            }
+                        var name = msg.substring(space + 2);
+                        var user = basicBot.userUtilities.lookupUserName(name);
                         
                         var randomnumber=Math.floor(Math.random()*6)
                         if(randomnumber === 1){
